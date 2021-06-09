3afca24dfef41a4b391298218097e2a6d745d2f8
Tanya <singhaltanya61@gmail.com>
2021-06-09 15:49:31 +0530

    async and callbacks

---
+++ Module 1/Functions/Function.js
@@ -0,0 +1,10 @@
+function callMe(name){
+    console.log("Call me Function called!!")
+    console.log(name + "says Hii !!!");
+    return 10;
+}
+
+//function invocation/call
+console.log(callMe);
+callMe();
+ 

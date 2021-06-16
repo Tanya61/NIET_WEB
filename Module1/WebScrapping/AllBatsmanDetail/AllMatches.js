6a96746b5384e5a75624e4a79b1ce6d97fc3027a
Tanya <singhaltanya61@gmail.com>
2021-06-16 06:00:04 +0530

    AllBatsman Details modified

--- Module1/WebScrapping/AllBatsmanDetail/AllMatches.js
+++ Module1/WebScrapping/AllBatsmanDetail/AllMatches.js
@@ -1,18 +1,20 @@
 let allMatchesLink = 'https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results';
-
 const request = require("request");
 const cheerio = require("cheerio");
 const getMatchDetails = require("./match");
 
-request(allMatchesLink, function(error, res, data){
+request(allMatchesLink , function(err , res , data){
     processHTML(data);
 })
 
+
 function processHTML(data){
     let ch = cheerio.load(data);
-    let AllATags = ch('a[data-hover="Scorecard"]');
-    for(let i=0;i<AllATags.length;i++){
-        let matchLink = "https//www.espncricinfo.com"+ch(AllATags[i]).attr("href");
+
+    let allATags = ch('a[data-hover="Scorecard"]');
+    for(let i=0 ; i<allATags.length ; i++){
+        let matchLink = "https://www.espncricinfo.com"+ch(allATags[i]).attr("href");
+        // console.log(matchLink);
         getMatchDetails(matchLink);
     }
 }

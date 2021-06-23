31f456dfc87ca4ba80cfb52b8054db68dc7f03c2
Tanya <singhaltanya61@gmail.com>
2021-06-23 15:26:51 +0530

    Dom implemented

--- Module1/9.AsyncAwait/assignment.js
+++ Module1/9.AsyncAwait/assignment.js
@@ -1,8 +1,11 @@
 const puppeteer = require("puppeteer");
+let name = "anika mehta";
 let id = "mifif53635@beydent.com";
 let pw = "12347890";
+let dob = "19 june 2006";
+let search = "pepcoding";
 
-(async function (){
+(async function(){
     try{
         let browser = await puppeteer.launch({
             headless: false,
@@ -11,14 +14,13 @@ let pw = "12347890";
         });
         let pages = await browser.pages();
         let tab = pages[0];
-        await tab.goto("https://www.hackerrank.com/auth/login");
-        await tab.type("#input-1", id);
-        await tab.type("#input-2", pw);
-        await tab.click(".ui-btn.ui-btn-large");
-        await tab.click("#base-card-1-link");
-        
+        await tab.goto("https://www.facebook.com/login");
+        await tab.type("#email_container", id);
+        await tab.type("#pass", pw);
+        await tab.click("#loginbutton");
+        await tab.type(".hv4rvrfc .b5q2rw42 .k4urcfbm .oajrlxb2", search);
     }
-    catch(err){
+    catch{
         console.log(err);
     }
 })();

4b766707fedba013b4145193f2d4a9d125ac0196
Tanya <singhaltanya61@gmail.com>
2021-07-14 19:03:15 +0530

    input in cell

--- Module2/ExcelClone/script.js
+++ Module2/ExcelClone/script.js
@@ -1,6 +1,7 @@
 let rowNumSec = document.querySelector(".row-num-sec");
 let formulaBarSelectedCellArea = document.querySelector(".selected-cell");
 let lastCell;
+let dataObj = {};
 let cellSec = document.querySelector(".cell-sec");
 let columnTagsSection = document.querySelector(".column-tag-sec");
 
@@ -16,8 +17,6 @@ for(let i=1;i<=100;i++){
     rowNumSec.append(div);
 }
 
-
-
 for(let i=0;i<26;i++){
     let ASCII = 65+i;
     let reqAlphabet = String.fromCharCode(ASCII);
@@ -27,22 +26,39 @@ for(let i=0;i<26;i++){
     columnTagsSection.append(div);
 }
 
+//inside this nested for loop we are creating individual cells UI + cell obj
 for(let i=1;i<=100;i++){
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
     
-                       //i = 1 [A1,B1..........Z1]
-                       //i = 2 []
-                       //.
-                       //.
-                       //i = 100 [A100.........z100]
+                       
     for(let j=0;j<26;j++){      //i = 100   j = 25  asciiCode = 65+25=90  alpha = z  cellAdd = Z100
         // A to Z
         let AsciiCode = 65+j;
         let reqAlphabet = String.fromCharCode(AsciiCode);
         let cellAdd = reqAlphabet+i;
 
+        dataObj[cellAdd] = {
+            value: undefined,
+            formula: undefined,
+            upstream : [],
+            downstream : [],
+        };
+
         let cellDiv = document.createElement("div");
+
+        cellDiv.addEventListener("input", function(e){
+            // jis cell pr type kra uske attribute se maine uska cell address fetch kra
+            let currCellAddres = e.currentTarget.getAttribute("data-address");
+
+            //kuki sare cell objects dataObj me store ho rakhe h using their cell address as key
+            //maine jis cell pr click krke type kra uska hi address fetch and uska hi object chahiye
+            //to wo address as key use krke dataObj se fetch krlia req cellObj ko
+            let currCellObj = dataObj[currCellAddres];
+            currCellObj.value = e.currentTarget.innerText;
+            console.log(currCellObj);
+        })
+
         cellDiv.setAttribute("contentEditable", true);
         cellDiv.classList.add("cell");
         cellDiv.setAttribute("data-address", cellAdd);

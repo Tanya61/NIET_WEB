50a51aa3496ca40d96b27d474d66cf1608db17ab
Tanya <singhaltanya61@gmail.com>
2021-08-09 09:16:38 +0530

    Excel almost complete

--- Module2/ExcelClone/StyleScript.js
+++ Module2/ExcelClone/StyleScript.js
@@ -1,9 +1,18 @@
 let AllAlignOptions = document.querySelectorAll(".align span");
 let AllStyleOptions = document.querySelectorAll(".text-style span");
+let AllColorOptions = document.querySelectorAll(".color span");
+
+let menuBarOption = document.querySelectorAll(".menu-bar div");
+let fileOptions = menuBarOption[0];
+
+let body = document.querySelector("body");
 
 let leftAlign = AllAlignOptions[0];
 let centerAlign = AllAlignOptions[1];
 let rightAlign = AllAlignOptions[2];
+
+let bgColorPicker = AllColorOptions[0];
+let fontColorPicker = AllColorOptions[1];
 
 leftAlign.addEventListener("click", function(){
     if(lastCell){
@@ -48,5 +57,65 @@ italicsText.addEventListener("click", function(){
 ulineText.addEventListener("click", function(){
     if(lastCell){
         lastCell.style.textDecoration = "underline";
+    }
+});
+
+bgColorPicker.addEventListener("click", function(){
+    let colorPickerElement = document.createElement("input");
+    colorPickerElement.type = "color";
+    body.append(colorPickerElement);
+    colorPickerElement.click();
+
+    colorPickerElement.addEventListener("input", function(e){
+        console.log(e.currentTarget.value);
+        if(lastCell){
+            lastCell.style.backgroundColor = e.currentTarget.value;
+            let address = lastCell.getAttribute("data-address");
+            dataObj[address].bgColor = e.currentTarget.value;
+        }
+    });
+});
+
+fontColorPicker.addEventListener("click", function(){
+    let colorPickerElement = document.createElement("input");
+    colorPickerElement.type = "color";
+    body.append(colorPickerElement);
+    colorPickerElement.click();
+
+    colorPickerElement.addEventListener("input", function(e){
+        console.log(e.currentTarget.value);
+        if(lastCell){
+            lastCell.style.color = e.currentTarget.value;
+            let address = lastCell.getAttribute("data-address");
+            dataObj[address].color = e.currentTarget.value;
+        }
+    });
+});
+
+fileOptions.addEventListener("click", function(){
+    let isOpen = fileOptions.getAttribute("data-open");
+    if(isOpen == "true"){
+        fileOptions.setAttribute("data-open", "false");
+        document.querySelector(".file-drop-down").remove();
+    }
+    else{
+        fileOptions.setAttribute("data-open", "true");
+
+        let dropDown = document.createElement("div");
+        dropDown.innerHTML = "<p>Save</p><p>Clear</p>";
+
+        let AllOptions = dropDown.querySelectorAll("p");
+        
+        AllOptions[0].addEventListener("click", function(){
+            localStorage.setItem("sheet", JSON.stringify(dataObj));
+
+        });
+
+        AllOptions[1].addEventListener("click", function(){
+            localStorage.setItem("sheet", "");
+        });
+
+        dropDown.classList.add("file-drop-down");
+        fileOptions.append(dropDown);
     }
 })
